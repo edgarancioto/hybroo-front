185bd39b2c0b2417a90a2212e097ad012154603e
Diony Borges <diony.borges.ribeiro@gmail.com>
2021-03-24 22:45:31 -0300

    Add WebSocket

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,76 @@
+const socket = new WebSocket('ws://hybroo2.herokuapp.com/0.0.0.0'); // Local: "ws://localhost:5000"
+
+socket.addEventListener('open', function (event) {
+    console.log('Connected to the WS Server!')
+    socket.send(JSON.stringify({ 'task': 'functions_names', 'params': 'None' }));
+});
+
+socket.addEventListener('close', function (event) {
+    console.log('Disconnected from the WS Server!')
+});
+
+socket.addEventListener('message', function (event) {
+    const { task } = JSON.parse(event.data)
+
+    switch (task) {
+        case "functions_names":
+            let { data } = JSON.parse(event.data)
+            data.forEach(function (item) {
+                $('#function_selected').append('<option  value="' + item.id + '">' + item.name + '</option>');
+            });
+            socket.send(JSON.stringify({ 'task': 'functions_methods', 'params': 'None' }));
+            break;
+
+        case "functions_details_img":
+            const { img } = JSON.parse(event.data)
+            $("#image-details").attr("src", img);
+            break;
+
+        case "functions_details":
+            populateCardContainerTwo(JSON.parse(event.data))
+            break;
+
+        case "functions_methods":
+            const xsd = JSON.parse(event.data)
+            // Popula single methos
+            jsonToArray(xsd).map(function (item) {
+                $('#method_single').append('<option  value="' + item[0] + '">' + item[1].name + '</option>');
+            });
+
+            // Popula hybrid methods
+            jsonToArray(xsd).map(function (item) {
+                if (item[1].approach === "HYBRID") {
+                    $('#method_hybrid').append('<option  value="' + item[0] + '">' + item[1].name + '</option>');
+                }
+            });
+
+            functionMethod = jsonToArray(xsd)
+            break;
+
+        case "functions_solver":
+            console.log(JSON.parse(event.data))
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+// Popula card do container two
+function populateCardContainerTwo(value) {
+    labels = Object.keys(value)
+    for (var i in labels) {
+        $("#list").append(`
+            <tr>
+                <td class="font-weight-bold capitalize pr-3 vertical-top">${labels[i]}</td>
+                <td class="vertical-top" >${value[labels[i]]}</td>
+            </tr>
+        `);
+    }
+    switchTwo()
+}
+
 let optionsFunciton;
 
 var functionMethod = []; // Recebe da APi todos do methods
@@ -15,6 +88,7 @@ var secondMethod = []
 
 // Escuta o checkbox "Single x Hybrid"
 $('#single-or-hybrid-switch').change(function () {
+    $("#fields-of-methods-hybrid").empty();
     if (document.getElementById("single-or-hybrid-switch").checked) {
         $('#method_hybrid_label').css('display', 'block')
         $('#method_hybrid').css('display', 'block')
@@ -32,7 +106,7 @@ $('#method_single').change(function () {
     // Limpa os input anterior para dar espaço a novos
     $("#fields-of-methods-single").empty();
 
-    // Busca o elemento que contem os valores 
+    // Busca o elemento que contem os valores
     let methodSelecionado = functionMethod.find((item) => {
         if (item[0] === this.value) {
             return item
@@ -65,44 +139,10 @@ $('#method_hybrid').change(function () {
     });
 });
 
-// Chama a API e popula o select do inicio
-async function callApi() {
-    axios.get(BASE_URL_API + 'functions-names')
-        .then(function (response) {
-            optionsFunciton = response.data.data;
-            optionsFunciton.forEach(function (item) {
-                $('#function_selected').append('<option  value="' + item.id + '">' + item.name + '</option>');
-            });
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    populationSelectOfMethods()
-}
 
 function getDetails() {
-    axios.get(BASE_URL_API + 'functions-details?id=' + valueSelected)
-        .then(function (response) {
-            getImage();
-
-            detailsSelected = response.data
-
-            switchTwo()
-
-            detailsLabels = Object.keys(detailsSelected)
-
-            for (var i in detailsLabels) {
-                $("#list").append(`
-                    <tr>
-                        <td class="font-weight-bold capitalize pr-3 vertical-top">${detailsLabels[i]}</td>
-                        <td class="vertical-top" >${detailsSelected[detailsLabels[i]]}</td>
-                    </tr>
-                `);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    socket.send(JSON.stringify({ 'task': 'functions_details', 'params': { 'function_id': valueSelected } }));
+    socket.send(JSON.stringify({ 'task': 'functions_details_img', 'params': { 'function_id': valueSelected } }));
 }
 
 function sendData() {
@@ -130,46 +170,11 @@ function sendData() {
     collectionData.firstMethod = firstMethod
     collectionData.secondMethod = secondMethod
 
+    socket.send(JSON.stringify({ 'task': 'functions_solver', 'params': { collectionData } }));
     console.log(collectionData)
 }
 
-// Popula select input com valores retornados da API
-async function populationSelectOfMethods() {
-    functionMethod = await axios.get(BASE_URL_API + 'functions-methods')
-        .then(function (response) {
-            // Popula single methos
-            jsonToArray(response.data).map(function (item) {
-                $('#method_single').append('<option  value="' + item[0] + '">' + item[1].name + '</option>');
-            });
-
-            // Popula hybrid methods
-            jsonToArray(response.data).map(function (item) {
-                if (item[1].approach === "HYBRID") {
-                    $('#method_hybrid').append('<option  value="' + item[0] + '">' + item[1].name + '</option>');
-                }
-            });
-
-            // Guarda valores para serem usados proteriormente
-            return jsonToArray(response.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-            return;
-        });
-}
-
-
-
-function getImage() {
-    axios.get(BASE_URL_API + 'functions-details-img?id=' + valueSelected)
-        .then(function (response) {
-            $("#image-details").attr("src", response.data.img);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
+// Funções que altera estado, mudança de layout, limpar estados.
 function switchTwo() {
     $('#first-container').css('display', 'none')
     $('#second-container').css('display', 'block')
@@ -196,14 +201,13 @@ $('#function_selected').on('change', function () {
     labelSelected = $(this).find('option').filter(':selected').text();
 });
 
-
-// :::::::::::::::::::::: Customs ::::::::::::::::::::::  
+// :::::::::::::::::::::: Customs ::::::::::::::::::::::
 //Passa Objeto em JSON recebi da API para array
 function jsonToArray(obj) {
     return Object.keys(obj).map((key) => [key, obj[key]]);
 }
 
-// :::::::::::::::::::::: Resets :::::::::::::::::::::: 
+// :::::::::::::::::::::: Resets ::::::::::::::::::::::
 $('#method_single').on('change', function () {
     $("#btn_next_3").prop("disabled", false);
     firstMethod = []
