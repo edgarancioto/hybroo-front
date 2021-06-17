3ae3b8b00597d91f46de640d83bd282354c9d3b8
Tiago Araujo <tiago.araujo.web@gmail.com>
2021-06-17 15:19:02 -0300

    configurando instancias metodos de envio

--- src/pages/PageApp/InstancesProblems.jsx
+++ src/pages/PageApp/InstancesProblems.jsx
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import Select from "react-select";
 import { WebsocketsContext } from "../../context/useWebsockets";
-
+import { Link } from "react-router-dom";
+import Switch from "react-switch";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import EditRoundedIcon from "@material-ui/icons/EditRounded";
 import * as S from "./styles";
 
 function InstanceProblems() {
@@ -9,7 +12,13 @@ function InstanceProblems() {
   const [selectIntance, setSelectIntance] = useState([]);
   const [selectOptionsIntance, setSelectOptionsIntance] = useState([]);
   const [selectOptions, setSelectOptions] = useState();
-  const [selectTypeIntance, setSelectTypeInstance] = useState();
+  const [selectInfo, setSelectInfo] = useState([]);
+  const [selectFirstMethod, setSelectFirstMethod] = useState([]);
+  const [selectSecondMethod, setSelectSecondMethod] = useState([]);
+  const [optionsMethods, setOptionsMethods] = useState([]);
+  const [checkHybrid, setCheckHybrid] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [isResult, setIsResult] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
 
   const { sendMessage, response } = useContext(WebsocketsContext);
@@ -27,50 +36,149 @@ function InstanceProblems() {
       options={selectOptionsIntance}
       isDisabled={isDisabled}
       onChange={handleChangeOptions}
-
     />
   );
 
-  // function getDetails(id) {
-  //   sendMessage(
-  //     JSON.stringify({
-  //       task: "functions_details",
-  //       params: { function_id: id },
-  //     })
-  //   );
-  //   sendMessage(
-  //     JSON.stringify({
-  //       task: "functions_details_img",
-  //       params: { function_id: id },
-  //     })
-  //   );
-  // }
+  function getMethods() {
+    sendMessage(
+      JSON.stringify({
+        task: "instances_methods",
+        params: "None",
+      })
+    );
+  }
 
   function handleChangeInstance(event) {
-    let dataOptions = []
-    
-    arrayOptions.forEach((item, index) => {
-      if(item[0] === "task") return null
+    let dataOptions = [];
 
-      if(item[0] === event.label) {
+    arrayOptions.forEach((item, index) => {
+      if (item[0] === "task") return null;
+
+      if (item[0] === event.label) {
         item[1].forEach((data, index) => {
           dataOptions.push({ value: index, label: data });
         });
       }
-    })
+    });
 
     setSelectOptionsIntance(dataOptions);
   }
 
+  function handleSecondMethodFields(event) {
+    const { value } = event.target;
+
+    let newFields = [];
+
+    optionsMethods.forEach((item) => {
+      if (item[1].approach === "HYBRID") {
+        if (item[0] === value) {
+          newFields = item[1].fields;
+        }
+      }
+    });
+
+    setSelectSecondMethod(newFields);
+  }
+
   function handleChangeOptions(event) {
-    // getDetails(event.value);
-    // setSelectOptions(event);
-    
+    getMethods();
+    setSelectOptions(true);
     console.log(event);
   }
 
   function jsonToArray(obj) {
     return Object.keys(obj).map((key) => [key, obj[key]]);
+  }
+
+  function handleFirstMethodFields(event) {
+    const { value } = event.target;
+
+    let newFields = [];
+    optionsMethods.forEach((item) => {
+      if (item[0] === value) {
+        newFields = item[1].fields;
+      }
+    });
+
+    setSelectFirstMethod(newFields);
+  }
+
+  function handleCheckHybrid() {
+    setCheckHybrid(!checkHybrid);
+  }
+
+  function submitCollectionData() {
+    var optionSingle =
+      document.getElementById("option_single") !== null
+        ? document.getElementById("option_single").value
+        : "0";
+    var optionSecond =
+      document.getElementById("option_second") !== null
+        ? document.getElementById("option_second").value
+        : "0";
+
+    var fieldsFirtsMethod = [{ label: "first-method", value: optionSingle }];
+    var fieldsSecondMethod = [{ label: "second-method", value: optionSecond }];
+
+    if (selectFirstMethod.length > 0) {
+      selectFirstMethod.forEach((item, index) => {
+        var inputValue = document.getElementById("firstMethod" + index);
+        let label = item.label.split(" ")[0];
+        let value = inputValue.value;
+        let element = { label, value };
+        fieldsFirtsMethod.push(element);
+      });
+    }
+
+    if (checkHybrid) {
+      if (selectSecondMethod.length > 0) {
+        selectSecondMethod.forEach((item, index) => {
+          var inputValue = document.getElementById("secondMethod" + index);
+          let label = item.label.split(" ")[0];
+          let value = inputValue.value;
+          let element = { label, value };
+          fieldsSecondMethod.push(element);
+        });
+      }
+    }
+
+    let collectionData = {
+      //problem: selectInfo[0][1].toString(),
+      
+      isHybrid: checkHybrid,
+      firstMethod: fieldsFirtsMethod,
+      secondMethod: fieldsSecondMethod,
+    };
+
+    sendMessage(
+      JSON.stringify({
+        task: "instances_solver",
+        params: { collectionData },
+      })
+    );
+
+    if (selectFirstMethod.length > 0) {
+      setIsSubmit(true);
+    }
+  }
+
+  function confirmOptions() {
+    getMethods();
+    setIsDisabled(!isDisabled);
+
+    setSelectFirstMethod([]);
+    setSelectSecondMethod([]);
+    setIsSubmit(false);
+  }
+
+  function newRequestProblem() {
+    getMethods();
+    setIsDisabled(!isDisabled);
+    setSelectFirstMethod([]);
+    setSelectSecondMethod([]);
+
+    setIsResult(false);
+    setIsSubmit(false);
   }
 
   useEffect(() => {
@@ -89,12 +197,21 @@ function InstanceProblems() {
         });
 
         setSelectIntance(dataOptions);
+        break;
+      case "instances_methods":
+        let methods = jsonToArray(response);
+        setOptionsMethods(methods);
+        break;
 
+      case "instances_solver_results":
+        setIsResult(true);
         break;
 
       default:
         break;
     }
+
+    console.log(response);
   }, [response]);
 
   return (
@@ -104,7 +221,147 @@ function InstanceProblems() {
         <S.FunctionContent>{SelectIntances()}</S.FunctionContent>
 
         <S.FunctionContent>{SelectOptionsIntances()}</S.FunctionContent>
+
+        {selectOptions !== undefined ? (
+          <S.ContentButtons>
+            <S.ButtonsCard onClick={() => confirmOptions()}>
+              {isDisabled ? (
+                <>
+                  <EditRoundedIcon /> <spam>To Edit</spam>
+                </>
+              ) : (
+                <>
+                  <AddBoxIcon /> <spam>Confirm</spam>
+                </>
+              )}
+            </S.ButtonsCard>
+          </S.ContentButtons>
+        ) : null}
       </S.FunctionContainer>
+
+      {isDisabled ? (
+        <S.FunctionContainer>
+          <S.FunctionTittle>Options</S.FunctionTittle>
+          <S.FunctionContent>
+            <S.OptionsContent>
+              <div>
+
+                <span>First Method</span>
+                <select
+                  class="form-control"
+                  name="first-method"
+                  id="option_single"
+                  onChange={(e) => handleFirstMethodFields(e)}
+                >
+                  <option disabled="" selected="" value="0">
+                    - select an option -
+                  </option>
+                  {optionsMethods.map((item) => {
+                    if (item[0] === "task") return null;
+                    return <option value={item[0]}>{item[1].name}</option>;
+                  })}
+                </select>
+
+                <S.FieldsFirstMethod>
+                  {selectFirstMethod.map((item, index) => {
+                    return (
+                      <>
+                        <label>{item.label}</label>
+                        <input
+                          type={item.type}
+                          name={item.label}
+                          step={item.step}
+                          defaultValue={item.default}
+                          min={item.min}
+                          class="form-control"
+                          id={"firstMethod" + index}
+                        />
+                      </>
+                    );
+                  })}
+                </S.FieldsFirstMethod>
+              </div>
+
+              <div>
+                <S.ButtonSwitch>
+                  <spam>SINGLE</spam>
+                  <Switch
+                    onChange={() => handleCheckHybrid()}
+                    checked={checkHybrid}
+                    height={20}
+                    width={40}
+                    offColor="#eee"
+                    onColor="#eee"
+                    onHandleColor="#208ccc"
+                    offHandleColor="#208ccc"
+                  />
+                  <spam>HYBRID</spam>
+                </S.ButtonSwitch>
+
+                {checkHybrid ? (
+                  <>
+                    <span style={{ marginTop: "32px" }}>Second Method</span>
+                    <select
+                      class="form-control"
+                      name="second-method"
+                      id="option_second"
+                      onChange={(e) => handleSecondMethodFields(e)}
+                    >
+                      <option disabled="" selected="" value="0">
+                        - select an option -
+                      </option>
+                      {optionsMethods.map((item) => {
+                        if (item[0] === "task") return null;
+                        if (item[1].approach !== "HYBRID") return null;
+                        return <option value={item[0]}>{item[1].name}</option>;
+                      })}
+                    </select>
+
+                    <S.FieldsSecondMethod id="group-first-method">
+                      {selectSecondMethod.map((item, index) => {
+                        return (
+                          <>
+                            <label>{item.label}</label>
+                            <input
+                              type={item.type}
+                              name={item.label}
+                              step={item.step}
+                              defaultValue={item.default}
+                              min={item.min}
+                              class="form-control"
+                              id={"secondMethod" + index}
+                            />
+                          </>
+                        );
+                      })}
+                    </S.FieldsSecondMethod>
+                  </>
+                ) : null}
+              </div>
+            </S.OptionsContent>
+          </S.FunctionContent>
+          <S.WrapperButtons>
+            {!isSubmit ? (
+              <S.SubimitButton onClick={() => submitCollectionData()}>
+                Submit Function
+              </S.SubimitButton>
+            ) : !isResult ? (
+              <S.StartRequest>
+                <span>Start new execution... </span>
+                <strong>This may take a few minutes</strong>
+              </S.StartRequest>
+            ) : (
+              <S.StartRequest>
+                <span>Finishing the execution... </span>
+                <Link to="/recent">
+                  <strong> Show request</strong>
+                </Link>
+                <button onClick={() => newRequestProblem()}>New Request</button>
+              </S.StartRequest>
+            )}
+          </S.WrapperButtons>
+        </S.FunctionContainer>
+      ) : null}
     </>
   );
 }
